10141f2201eeeb980bbdb1211d493a50c60cca86
Vishal <vishalcodes26@gmail.com>
2025-01-29 22:32:01 +0530

    Changed some static elements into dynamic elements and also updated the img size of the skills

--- src/app/projects/projects.component.ts
+++ src/app/projects/projects.component.ts
@@ -8,5 +8,45 @@ import { Component } from '@angular/core';
   styleUrl: './projects.component.css'
 })
 export class ProjectsComponent {
+ projects: {
+  id: number;
+  imgName: string;
+  title: string;
+  repoName: string;
+  liveLink: string;
+
+ }[] = [
+  {
+    id: 1,
+    imgName: "tindog.png",
+    title: "Tin Dog",
+    repoName: "Tin-dog",
+    liveLink: "Tin-dog/"
+   },
+   {
+    id: 2,
+    imgName: "Drums.png",
+    title: "Drum Kit",
+    repoName: "drum-kit",
+    liveLink: "drum-kit"
+   },
+   {
+    id: 3,
+    imgName: "password.png",
+    title: "Password Generator",
+    repoName: "Password_generator",
+    liveLink: "Password_generator"
+   },
+ ];
+
+ openGithub(repoName: string) {
+
+  window.open(`https://github.com/vishalManowharan/${repoName}`);
+
+}
+
+openLiveLink(liveLink: string) {
+  window.open(`https://vishalmanowharan.github.io/${liveLink}`);
+}
 
 }
